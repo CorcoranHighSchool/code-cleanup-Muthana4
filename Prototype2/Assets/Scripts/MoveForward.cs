15db2e7b1fae6454d76349b32798363f024b4682
Muthana4 <142622962+Muthana4@users.noreply.github.com>
2024-04-08 11:47:54 -0700

    Remove unused libraries, Serialized public property, removed unused method. Optimized Vector3 Math. MoveForward.cs

--- Prototype2/Assets/Scripts/MoveForward.cs
+++ Prototype2/Assets/Scripts/MoveForward.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
+    [serializeField] private float speed= 40.0f;
     public float speed = 40.0f;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +14,6 @@ public class MoveForward : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * (speed * Time.deltaTime));
     }
 }
