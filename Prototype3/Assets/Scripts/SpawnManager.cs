ff6ce8db6387bf4f5f111bdd501cdd030637ddd6
Muthana4 <142622962+Muthana4@users.noreply.github.com>
2024-04-09 12:03:30 -0700

    removed un used libraries, serialize public.

--- Prototype3/Assets/Scripts/SpawnManager.cs
+++ Prototype3/Assets/Scripts/SpawnManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     //Prefab for the obstacle to spawn
-    public GameObject obstaclePrefab;
+    [SerializeField] private GameObject obstaclePrefab;
     //Spawn Position
     private Vector3 spawnPos = new Vector3(25.0f, 0.0f, 0.0f);
 
